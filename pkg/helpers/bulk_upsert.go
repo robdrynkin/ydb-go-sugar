c65061c9b5badc4fa9e3d3021da15fbc2bec919a
Aleksei Pleshakov <qrort@ydb.tech>
2025-03-04 16:19:20 +0100

    pass table.Options to bulk upsert

--- pkg/helpers/bulk_upsert.go
+++ pkg/helpers/bulk_upsert.go
@@ -6,10 +6,10 @@ import (
 	"github.com/ydb-platform/ydb-go-sdk/v3/table"
 )
 
-func (t *TableSugar[T]) BulkUpsert(ctx context.Context, c table.Client, dbName string, rows []T) error {
+func (t *TableSugar[T]) BulkUpsert(ctx context.Context, c table.Client, dbName string, rows []T, opts ...table.Option) error {
 	vals, err := t.ToListStructValues(rows)
 	if err != nil {
 		return err
 	}
-	return c.BulkUpsert(ctx, fmt.Sprintf("%s/%s", dbName, t.TableName), table.BulkUpsertDataRows(vals))
+	return c.BulkUpsert(ctx, fmt.Sprintf("%s/%s", dbName, t.TableName), table.BulkUpsertDataRows(vals), opts...)
 }
